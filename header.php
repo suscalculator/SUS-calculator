e5e1339454b30dce4d3544e2c722e548f950d019
Olawaldroid <91216404+Olawaldroid@users.noreply.github.com>
2025-01-30 13:39:57 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!-- header.php -->
+<!-- This file contains metadata and external resource links (CSS, JS) -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>SUS Questionnaire</title>
+<!-- Bootstrap CSS for styling -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<!-- Bootstrap JS for interactivity -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
