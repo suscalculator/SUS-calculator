cee74fc972cfed2398e67eded07e3b550a5475c5
Olawale Adediran <91216404+Olawaldroid@users.noreply.github.com>
2025-02-06 13:53:50 +0000

    Update script.js
    add code to prevent duplicate submission

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ function validateForm() {
     }
     return formValid;
 }
+    document.addEventListener("DOMContentLoaded", function () {
+        const form = document.querySelector("form");
+        form.addEventListener("submit", function () {
+            if (!form.dataset.submitted) {
+                form.dataset.submitted = "true";
+            } else {
+                event.preventDefault(); // Prevent duplicate submission
+            }
+        });
+    });
+</script>
