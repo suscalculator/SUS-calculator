f3d4193b5cfe61b4d83d8a074d9c279e02698243
Olawale Adediran <91216404+Olawaldroid@users.noreply.github.com>
2025-04-03 15:04:36 +0100

    Update questions.php

--- questions.php
+++ questions.php
@@ -26,6 +26,7 @@ foreach ($questions as $index => $question) {
     echo '<div class="radio-group">';
     
     // First radio button with "Strongly Disagree" label
+     echo '<div class="middle_checkbox">';
     echo '<label class="radio-label">';
      //echo '<span style="font-size: 12px;">Strongly Agree</span>';
     echo '<input type="radio" name="q' . ($index + 1) . '" value="1" required>';
@@ -41,6 +42,7 @@ foreach ($questions as $index => $question) {
     echo '</div>';
     
     // Last radio button with "Strongly Agree" label
+    echo '<div class="middle_checkbox">';
     echo '<label class="radio-label">';
      //echo '<span style="font-size: 12px;">Strongly Agree</span>';
     echo '<input type="radio" name="q' . ($index + 1) . '" value="5" required>';
