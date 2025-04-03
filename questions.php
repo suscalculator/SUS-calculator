0941bc5c602a4361bc321e3ed3eb26c73cd0c62b
Olawale Adediran <91216404+Olawaldroid@users.noreply.github.com>
2025-04-03 15:07:46 +0100

    Update questions.php

--- questions.php
+++ questions.php
@@ -34,6 +34,7 @@ foreach ($questions as $index => $question) {
     
     // Middle radio buttons without labels
    //  echo '<div class="middle_checkbox">';
+    echo '<div class="radio-label">';
     for ($i = 2; $i <= 4; $i++) {
         echo '<label class="radio-label">';
         echo '<input type="radio" name="q' . ($index + 1) . '" value="' . $i . '" required>';
