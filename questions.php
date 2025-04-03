ac9fe310b65149adfcdbe65d5311527a11916cf0
Olawale Adediran <91216404+Olawaldroid@users.noreply.github.com>
2025-04-03 14:45:30 +0100

    Update questions.php

--- questions.php
+++ questions.php
@@ -16,6 +16,12 @@ foreach ($questions as $index => $question) {
     echo '<div class="form-group">';
     echo '<label>' . ($index + 1) . '. ' . $question . '</label>';
     echo '<div class="check-box-lavel"></div>'; // Empty div for spacing or styling purposes
+        // Flex row with "Very Good" and "Poor"
+    echo '<div class="flex-row" style="display: flex; justify-content: space-between; font-size: 12px; margin: 4px 0;">';
+    echo '<span>Strongly Disagree</span>';
+    echo '<span>Strongly Agree</span>';
+    echo '</div>';
+    
     
     echo '<div class="radio-group">';
     
